43c0870044c924f9df44ee6a02551da4f93a9b35
unknown <sabinorotondi2000@gmail.com>
2022-07-20 00:08:04 +0200

    feat: create and use ecmascript ex done

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import sum from "./sum.mjs";
+sum();
+
+//used mjs to let node know we are working with ECMAScript modules, we could leave the js extension by modifying the package.json: ("type" : "module")
---
+++ sum.mjs
@@ -0,0 +1,4 @@
+const sum = () => console.log(2+7)
+export default sum;
+
+//used mjs to let node know we are working with ECMAScript modules, we could leave the js extension modifying the package.json: ("type" : "module")
